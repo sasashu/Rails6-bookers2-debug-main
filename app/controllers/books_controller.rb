f70045c9911902f011a7161379885010cce6d08e
EC2 Default User <ec2-user@ip-172-31-38-185.ap-northeast-1.compute.internal>
2022-10-08 12:04:31 +0000

    [Add]いいね数昇順設定

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -12,6 +12,7 @@ class BooksController < ApplicationController
 
   def index
     @books = Book.all
+    @books = Book.includes(:favorited_users).sort {|a,b| b.favorited_users.size <=> a.favorited_users.size}
     @book = Book.new
   end
 
